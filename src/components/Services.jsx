f05effb5b58a78d7bae4c9a2c6019d082f90e543
James Joseph <98562132+JW796@users.noreply.github.com>
2025-08-14 12:42:09 -0400

    WIP 4 Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,12 @@ import Section from "./Section";
 import Heading from "./Heading";
 import { service1, service2, service3, check } from "../assets";
 import { brainwaveServices, brainwaveServicesIcons } from "../constants";
-import { PhotoChatMessage, Gradient, VideoBar, VideoChatMessage } from "./design/Services";
+import {
+  PhotoChatMessage,
+  Gradient,
+  VideoBar,
+  VideoChatMessage,
+} from "./design/Services";
 
 import Generating from "./Generating";
 
@@ -48,7 +53,7 @@ const Services = () => {
           <Generating className="absolute left-4 right-4 bottom-4 border-n-1/10 border lg:left-1/2 lg:right-auto lg:bottom-0 lg:-translate-x-1/2" />
         </div>
 
-        <div className="relative z-1 grib gap-6 lg:grid-cols-2">
+        <div className="relative z-1 grid gap-6 lg:grid-cols-2">
           <div className="relative min-h-[39rem] border border-n-1/10 rounded-3xl overflow-hidden">
             <div className="absolute inset-0">
               <img
@@ -71,7 +76,53 @@ const Services = () => {
               <PhotoChatMessage />
             </div>
           </div>
+
+          <div className="p-4 bg-n-7 rounded-3xl overflow-hidden lg:min-h-[46rem]">
+            <div className="py-12 px-4 xl:px-8">
+              <h4 className="h4 mb-4">Video Generation</h4>
+              <p className="body-2 mb-[2rem] text-n-3">
+                The World's most powerful AI photo and video art generation
+                engine. What will you create?
+              </p>
+              <ul className="flex items-center justify-between">
+                {brainwaveServicesIcons.map((item, index) => (
+                  <li
+                    key={index}
+                    className={`rounded-2xl flex items-center justify-center ${
+                      index === 2
+                        ? "w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]"
+                        : "flex w-10 h-10 bg-n-6 md:w-15 md:h-15"
+                    }`}
+                  >
+                    <div
+                      className={
+                        index === 2
+                          ? "flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]"
+                          : ""
+                      }
+                    >
+                      <img src={item} width={24} height={24} alt="item" />
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="relative h-[20rem] bg-n-8 rounded-xl overflow-hidden md:h-[25rem]">
+              <img
+                className="w-full h-full object-cover"
+                src={service3}
+                width={520}
+                height={400}
+                alt="scary robat"
+              />
+
+              <VideoChatMessage />
+              <VideoBar />
+            </div>
+          </div>
         </div>
+
+        <Gradient />
       </div>
     </Section>
   );
