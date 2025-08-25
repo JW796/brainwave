92cf5346b3bf402ba57bbc494b9cbcdafcc16217
James Joseph <98562132+JW796@users.noreply.github.com>
2025-08-25 06:39:44 -0400

    WIP 3 Pricing

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,7 +4,7 @@ const Heading = ({ className, title, text, tag }) => {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {tag && (
-        <Tagline>
+        <Tagline className="mb-4 md:justify-center">
           {tag}
         </Tagline>
       )}
