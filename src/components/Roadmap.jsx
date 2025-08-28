84a831d0ead66195efb32d76341786fd7b770cd7
James Joseph <98562132+JW796@users.noreply.github.com>
2025-08-28 11:23:10 -0400

    Add Roadmap

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,56 @@
+import Section from "./Section";
+import Heading from "./Heading";
+import { roadmap } from "../constants";
+import Tagline from "./Tagline";
+import { grid, check2, loading1 } from "../assets";
+
+const Roadmap = () => {
+  return (
+    <Section className="overflow-hidden" id="roadmap">
+      <div className="container md:pb-10">
+        <Heading tag="Ready to get started" title="What we're working on" />
+      </div>
+
+      <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+        {roadmap.map((item) => {
+          const status = item.status === "done" ? "Done" : "In Progress";
+
+          return(
+          <div key={item.id}>
+            <div>
+              <div>
+                <img
+                  src={grid}
+                  className="w-full"
+                  width={550}
+                  height={550}
+                  alt="Grid"
+                />
+              </div>
+
+              <div className="relative z-1">
+                <div className="flex items-center justify-between">
+                  <Tagline>{item.date}</Tagline>
+                </div>
+
+                <div>
+                  <img
+                    src={item.status === "done" ? check2 : loading1}
+                    className="mr-2.5"
+                    width={16}
+                    height={16}
+                    alt={status}
+                  />
+                  <div className="tagline">{status}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          );
+        })}
+      </div>
+    </Section>
+  );
+};
+
+export default Roadmap;
